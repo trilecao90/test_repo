18ff78aa83493120f1690fd52f0c8db835edbd1d
trilecao90 <trilecao90@gmail.com>
2021-03-07 20:15:10 +0700

    for test purpose only

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 5;
+    int b = 3;
+    printf("Before: a=%d b=%d \n", a, b);
+    swap(&a, &b);
+    printf("After : a=%d b=%d \n", a, b);
+    return 0;
+}
